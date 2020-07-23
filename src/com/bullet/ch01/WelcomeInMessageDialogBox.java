b62c2462d0296e3f0c917be1fbc0b4a386535eef
bullet-gao <bullet.gao@outlook.com>
2020-07-23 16:06:30 +0800

    second commit

---
+++ src/com/bullet/ch01/WelcomeInMessageDialogBox.java
@@ -0,0 +1,18 @@
+package com.bullet.ch01;
+
+import javax.swing.JOptionPane;
+
+/**
+ * This application program displays Welcome to Java!
+ * in a message dialog box.
+ *
+ * @author GAOY85
+ *
+ */
+
+public class WelcomeInMessageDialogBox {
+    public static void main(String[] args) {
+        // Display Welcome to Java! in a message dialog box
+        JOptionPane.showMessageDialog(null, "Welcome to Java!");
+    }
+}
